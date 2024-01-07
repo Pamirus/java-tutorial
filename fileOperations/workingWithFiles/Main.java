f35db155acce7c9d64d352c44ba40626b3e1a73e
Pamirus <pamirfescioglu@gmail.com>
2024-01-07 18:10:48 +0300

    Initial commit

---
+++ fileOperations/workingWithFiles/Main.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\pamir\\Desktop\\Java\\java-tutorial\\fileOperations\\files\\students.txt");
+
+        createFile(file);
+        getFileInfo(file);
+        // removeFile(file);
+    }
+
+    public static void createFile(File file) {
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File is created.");
+            } else {
+                System.out.println("The file is already existing.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void getFileInfo(File file) {
+        if (file.exists()) {
+            System.out.println("File name: " + file.getName());
+            System.out.println("File path: " + file.getAbsolutePath());
+            System.out.println("File writable: " + file.canWrite());
+            System.out.println("File is readable: " + file.canRead());
+            System.out.println("File size: " + file.length());
+        } else {
+            System.out.println("The file does not exist");
+        }
+    }
+
+    public static void removeFile(File file) {
+        try {
+            if (file.delete()) {
+                System.out.println("File is removed.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
